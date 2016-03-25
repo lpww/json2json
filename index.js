239d333a33203352468d0daa425d9b0d5153ff91
lpww <t.treffry@gmail.com>
2016-03-24 21:31:43 -0700

    Remove coffeescript dependency

--- index.js
+++ index.js
@@ -1,6 +1,1 @@
-try {
-  module.exports = require('./compiled');
-} catch (err) {
-  require('coffee-script/register');
-  module.exports = require('./lib/json2json');
-}
+module.exports = require('./lib/json2json');
